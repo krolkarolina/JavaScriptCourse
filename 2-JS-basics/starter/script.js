998f78867bbe5f4b1c7f31dd5821aff3ffec527b
Karolina <krolkarolina.kk@gmail.com>
2018-07-23 14:42:43 +0200

    js variable and data types - basics

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,26 @@
+var firstName = "John";
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+//comment
+/*
+multiple line comment
+
+*/
+
+alert(firstName + ' is a ' + age + ' year old.');
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+
+console.log(typeof firstName);
