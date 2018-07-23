b5b0cad07b9aa931c341f55186dd246cb2734769
Karolina <krolkarolina.kk@gmail.com>
2018-07-23 14:43:53 +0200

    js ternary operator and switch statement

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,4 @@
+/*
 var firstName = "John";
 console.log(firstName);
 
@@ -17,10 +18,32 @@ console.log(job);
 /*
 multiple line comment
 
-*/
+
 
 alert(firstName + ' is a ' + age + ' year old.');
 var lastName = prompt('What is his last name?');
 console.log(firstName + ' ' + lastName);
 
 console.log(typeof firstName);
+*/
+
+// ternary operator and switch statement
+
+age >= 16 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+var job = 'teacher';
+
+switch(job) {
+  case 'teacher':
+    console.log(firstName + ' is a teacher.');
+    break;
+  case 'driver':
+    console.log(firstName + ' is a driver.');
+    break;
+  default:
+    console.log(firstName + ' does something else.');
+    break;
+}
