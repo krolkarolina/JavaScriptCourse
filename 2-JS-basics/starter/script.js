0e04e3678f659b367e0f3cb5c0b5f8988e21f48e
Karolina <krolkarolina.kk@gmail.com>
2018-07-23 14:47:38 +0200

    coding challenge 1 - BMI

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -28,7 +28,7 @@ console.log(typeof firstName);
 */
 
 // ternary operator and switch statement
-
+/*
 age >= 16 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
 
 var drink = age >= 18 ? 'beer' : 'juice';
@@ -47,3 +47,25 @@ switch(job) {
     console.log(firstName + ' does something else.');
     break;
 }
+*/
+
+
+
+//coding challenge 1
+//count BMI
+
+
+var massJohn = 70;
+var massMark = 100;
+
+var heightJohn = 1.78;
+var heightMark = 1.85;
+
+var bmiMark = massMark/(heightMark * heightMark);
+var bmiJohn = massJohn/(heightJohn*heightJohn);
+
+console.log("John BMI " + bmiJohn);
+console.log("Mark BMI " + bmiMark);
+
+var isMarkBMIHigherThanJohn = bmiMark > bmiJohn;
+console.log("Is Mark\'s BMI higher than John\'s? " + isMarkBmiHigherThanJohn)
